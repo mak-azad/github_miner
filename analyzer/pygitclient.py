748d07366cba0fd10754de72269b46eb823f7cc6
ssmtariq <ssmtariq@node0.repominer.cloudprof-pg0.utah.cloudlab.us>
2023-10-27 14:10:26 -0600

    Update pygitclient

--- analyzer/pygitclient.py
+++ analyzer/pygitclient.py
@@ -5,60 +5,61 @@ import os
 # Set Git Credentials
 username = ''
 token = ''  # You can use a Personal Access Token (PAT) as well
-email = ''
+email = 'syedtariqfiles@gmail.com'
+
+# def commit_n_push():
+
+#     # Generate the output file name with the specified prefix and IP address
+#     host_ip = socket.gethostbyname(socket.gethostname())
+#     output_csv_file = os.path.join(f"/users/{username}/github_miner/analyzer/results", f"github_repo_analysis_result_{host_ip}.csv")
+
+#     # Check if the output_csv_file exists
+#     if os.path.exists(output_csv_file):
+
+#         # Replace these values with your repository information
+#         repository_path = f'/users/{username}/github_miner'
+#         remote_name = 'origin'
+#         branch_name = 'main'  # Replace with your branch name
+
+#         # Open the repository
+#         repo = pygit2.Repository(repository_path)
+
+#         # Get the remote
+#         remote = repo.remotes[remote_name]
+
+#         # Fetch the latest changes from the remote
+#         remote.fetch()
+
+#         # Get the status of the repository
+#         status = repo.status()
+
+#         # Check if there are changes to commit
+#         if output_csv_file in status and status[output_csv_file] != pygit2.GIT_STATUS_CURRENT:
+#             # Create a new commit with a message
+#             index = repo.index
+#             index.add(output_csv_file)  # Only add the specific file
+#             index.write()
+#             tree = index.write_tree()
+#             author = pygit2.Signature(username, email)
+#             committer = author
+#             message = "Update result file from the host: " + host_ip
+#             commit_oid = repo.create_commit('HEAD', author, committer, message, tree, [repo.head.target])
+
+#             # Push the commit to the remote branch
+#             credentials = pygit2.UserPass(username, token)
+#             remote.push(["refs/heads/" + branch_name], callbacks=pygit2.RemoteCallbacks(credentials=credentials))
+
+#             print("Changes committed and pushed successfully.")
+#         else:
+#             print("No changes to commit.")
+#     else:
+#         print(f"The file {output_csv_file} does not exist. Skipping commit and push.")
+
 
 def commit_n_push():
 
-    # Generate the output file name with the specified prefix and IP address
     host_ip = socket.gethostbyname(socket.gethostname())
     output_csv_file = os.path.join(f"/users/{username}/github_miner/analyzer/results", f"github_repo_analysis_result_{host_ip}.csv")
-
-    # Check if the output_csv_file exists
-    if os.path.exists(output_csv_file):
-
-        # Replace these values with your repository information
-        repository_path = f'/users/{username}/github_miner'
-        remote_name = 'origin'
-        branch_name = 'main'  # Replace with your branch name
-
-        # Open the repository
-        repo = pygit2.Repository(repository_path)
-
-        # Get the remote
-        remote = repo.remotes[remote_name]
-
-        # Fetch the latest changes from the remote
-        remote.fetch()
-
-        # Get the status of the repository
-        status = repo.status()
-
-        # Check if there are changes to commit
-        if output_csv_file in status and status[output_csv_file] != pygit2.GIT_STATUS_CURRENT:
-            # Create a new commit with a message
-            index = repo.index
-            index.add(output_csv_file)  # Only add the specific file
-            index.write()
-            tree = index.write_tree()
-            author = pygit2.Signature(username, email)
-            committer = author
-            message = "Update result file from the host: " + host_ip
-            commit_oid = repo.create_commit('HEAD', author, committer, message, tree, [repo.head.target])
-
-            # Push the commit to the remote branch
-            credentials = pygit2.UserPass(username, token)
-            remote.push(["refs/heads/" + branch_name], callbacks=pygit2.RemoteCallbacks(credentials=credentials))
-
-            print("Changes committed and pushed successfully.")
-        else:
-            print("No changes to commit.")
-    else:
-        print(f"The file {output_csv_file} does not exist. Skipping commit and push.")
-
-def commit_n_push():
-
-    host_ip = socket.gethostbyname(socket.gethostname())
-    output_csv_file = os.path.join("results", f"github_repo_analysis_result_{host_ip}.csv")
 
     if os.path.exists(output_csv_file):
         repository_path = f'/users/{username}/github_miner'
