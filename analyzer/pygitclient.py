69921296850f7fe03fc9534ce9eb04b014c222cd
ssmtariq <ssmtariq@node0.repominer.cloudprof-pg0.utah.cloudlab.us>
2023-10-29 14:51:07 -0600

    Update gitignore

--- analyzer/pygitclient.py
+++ analyzer/pygitclient.py
@@ -84,7 +84,7 @@ def commit_and_push(repo, remote, output_csv_file, username, token, email, branc
     author = pygit2.Signature(username, email)
     committer = author
     message = "Update result file from the host: " + host_ip
-    print("Pulled latest remote changes: ", pull_changes(branch_name))
+    print("Pulled latest remote changes: ", pull_changes(repo, remote, branch_name))
     commit_oid = repo.create_commit('HEAD', author, committer, message, tree, [repo.head.target])
     credentials = pygit2.UserPass(username, token)
     repo.remotes['origin'].push(["refs/heads/" + branch_name], callbacks=pygit2.RemoteCallbacks(credentials=credentials))
