f55caa7c50a2b86a722a253700438eafca9a02fd
ssmtariq <ssmtariq@node0.repominer.cloudprof-pg0.utah.cloudlab.us>
2023-10-31 20:36:37 -0600

    Implement commit filter with predefined keywords list

--- analyzer/repo_analyzer.py
+++ analyzer/repo_analyzer.py
@@ -1,5 +1,6 @@
 import socket
 from pydriller import Repository
+from pydriller import Commit
 from pygitclient import commit_n_push
 import csv
 import os
@@ -11,7 +12,11 @@ published_commits = 0
 commit_counter = 0
 # Set the number of commits after which the result file will be pushed to GitHub
 buffer_size = 100
-# max_file_size = 4 * 1024  # 40MB
+
+general_keywords = ["deadlock", "race condition", "synchronization error", "mutex", "semaphore", "starvation", "locking", 
+                    "multiple", "concurrency", "concurrent", "parallel", "multithreading", "threading", "threads", "thread pool", 
+                    "multiprocessing", "await", "async", "atomic", "asyncio", "concurrent.futures"]
+concurrency_keywords = ["await", "awaitable" "async", "asyncio", "create_task", "semaphore", "threading", "multiprocessing", "concurrent.futures"]
 
 # Create a 'results' directory if it doesn't exist
 results_dir = f'{root_dir}/results'
@@ -27,34 +32,79 @@ def read_repository_urls_from_csv(input_csv_file):
         repo_urls = [row[6] for row in reader]
     return repo_urls
 
+def filter_commit(commit):
+    # Check if the commit message contains any keywords from general_keywords
+    if any(keyword in commit.msg for keyword in general_keywords):
+        modified_files = []
+
+        # Iterate through the modified files in the commit
+        for file in commit.modifications:
+            # Get the diff of each file
+            diff = file.diff
+
+            # Look for added lines in the diff
+            added_lines = [
+                line
+                for line in diff.split("\n")
+                if line.startswith("+") and not line.startswith("+++")
+            ]
+
+            # Check if any added line contains keywords from concurrency_keywords
+            if any(
+                keyword in line
+                for line in added_lines
+                for keyword in concurrency_keywords
+            ):
+                modified_files.append(file)
+
+        # Create a new Commit object with the filtered files
+        filtered_commit = Commit(
+            commit.hash,
+            commit.author.name,
+            commit.author.email,
+            commit.msg,
+            commit.author_date,
+            commit.committer_date,
+            modified_files,
+            commit.parents,
+        )
+
+        return filtered_commit
+
+    return None
+
 def analyze_repository(repo_url, output_csv_file):
     global commit_counter
     global published_commits
     commit_data = []
     for commit in Repository(repo_url, only_modifications_with_file_types=[file_type]).traverse_commits():
-        original_codes = []
-        modified_codes = []
-        modified_files = []
-        methods_before = []
-        methods_after = []
-        for m in commit.modified_files:
-            try:
-                original_codes.append(m.source_code_before)
-                modified_codes.append(m.source_code)
-                modified_files.append(m.filename)
-                methods_before.append(m.methods_before)
-                methods_after.append(m.changed_methods)
-            except ValueError as e:
-                print(f"Error processing commit {commit.hash}: {e}")
-                continue  # Continue with the next commit if an error occurs
+        filtered_commit = filter_commit(commit)
+        if filtered_commit is not None:
+            commit = filtered_commit
+            original_codes = []
+            modified_codes = []
+            modified_files = []
+            methods_before = []
+            methods_after = []
+            for m in commit.modified_files:
+                try:
+                    original_codes.append(m.source_code_before)
+                    modified_codes.append(m.source_code)
+                    modified_files.append(m.filename)
+                    methods_before.append(m.methods_before)
+                    methods_after.append(m.changed_methods)
+                except ValueError as e:
+                    print(f"Error processing commit {commit.hash}: {e}")
+                    continue  # Continue with the next commit if an error occurs
 
-        commit_data.append([commit.project_name, commit.hash, commit.msg, commit.committer_date, commit.author.name, commit.insertions, commit.deletions, commit.lines, commit.files, modified_files, original_codes, modified_codes, methods_before, methods_after])
-        commit_counter += 1
-        # Push interim commit analysis result to GitHub
-        if commit_counter % buffer_size == 0:
-            published_commits += buffer_size
-            write_commit_analysis_to_csv(output_csv_file, commit_data)
-            print(f"{commit_counter} commits are added")
+            commit_data.append([commit.project_name, commit.hash, commit.msg, commit.committer_date, commit.author.name, commit.insertions, 
+                                commit.deletions, commit.lines, commit.files, modified_files, original_codes, modified_codes, methods_before, methods_after])
+            commit_counter += 1
+            # Push interim commit analysis result to GitHub
+            if commit_counter % buffer_size == 0:
+                published_commits += buffer_size
+                write_commit_analysis_to_csv(output_csv_file, commit_data)
+                print(f"{commit_counter} commits are added")
     # Ensure all commits are written to result file
     if(commit_counter>published_commits):
         write_commit_analysis_to_csv(output_csv_file, commit_data)
@@ -85,23 +135,10 @@ def main():
     # Specify the output file path inside the 'results' directory
     output_csv_file = os.path.join(results_dir, f"github_repo_analysis_result_{host_ip}.csv")
 
-    # file_counter = 1
     for repo_url in repo_urls:
         # Analyze each repository and collect commit data
         print("Processing repo url: ", repo_url)
-        # current_file_size = os.path.getsize(output_csv_file)
-        # print(f'File {output_csv_file} size is {current_file_size} before the processing repo: {repo_url}')
         analyze_repository(repo_url, output_csv_file)
-
-        # current_file_size = os.path.getsize(output_csv_file)  # Update the current file size
-        # print(f'File {output_csv_file} size is {current_file_size} after the processing repo: {repo_url}')
-        # print('Max file size: {}')
-        # Roll the file to a new one if the size exceeds the limit
-        # if current_file_size > max_file_size:
-        #     roll_output_csv_file(output_csv_file, file_counter)
-        #     # commit_n_push(username=args.username, token=args.token, email=args.email)
-        #     current_file_size = 0  # Reset the current file size
-        #     file_counter += 1
 
     # Push all the changes in the analyzer/results directory to github
     # commit_n_push(username=args.username, token=args.token, email=args.email)
